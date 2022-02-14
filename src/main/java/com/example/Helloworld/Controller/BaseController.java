f228112b84b696816d5b2f426ddeb1437faecd1e
manhhung012 <32127425+manhhung012@users.noreply.github.com>
2022-02-14 16:28:56 +0700

    edit templates

--- src/main/java/com/example/Helloworld/Controller/BaseController.java
+++ src/main/java/com/example/Helloworld/Controller/BaseController.java
@@ -2,14 +2,14 @@ package com.example.Helloworld.Controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BaseController {
-    @RequestMapping("/home")
-    @ResponseBody
-    public ResponseEntity<String> homeBody(){
-        return ResponseEntity.ok("index");
+    @GetMapping
+    public String login(){
+        return "index";
     }
 }
