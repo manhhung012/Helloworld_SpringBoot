4a2fcff996391b4293f9db1843062e8240efc629
manhhung012 <32127425+manhhung012@users.noreply.github.com>
2022-02-15 16:00:09 +0700

    Update ProductController.java

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,4 +1,24 @@
 package com.example.controller;
 
+import com.example.dao.ProductDao;
+import com.example.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ProductController {
+
+    @Autowired
+    ProductDao productDao;
+
+    @GetMapping("/product")
+    public List<Product> getProduct(){
+
+        return productDao.findAll();
+    }
 }
